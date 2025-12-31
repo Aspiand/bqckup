337b293225b5f66de9686ce3b97942e5716b5b47
Aspian <muhammad.aspian.d@gmail.com>
2025-12-31 15:22:01 +0800

    remove

--- constant/__init__.py
+++ constant/__init__.py
@@ -15,7 +15,7 @@ CONFIG_PATH = path.join(BQ_PATH, 'bqckup.cnf')
 RUSTIC_CONFIG_PATH = "/etc/rustic"
 
 # Bqckup Information
-VERSION = "1.8.2"
+VERSION = "1.8.2-as"
 
 # YOURLS Credentials
 YOURLS_HOST = ""
